e39760f7a138c12ff22c06889339a9ec0fa951ba
Anwesh43 <anweshthecool0@gmail.com>
2018-03-22 08:14:21 +0530

    adding TicTacBoxView class

--- tictacboxview/src/main/java/ui/anwesome/com/tictacboxview/TicTacBoxView.kt
+++ tictacboxview/src/main/java/ui/anwesome/com/tictacboxview/TicTacBoxView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.tictacboxview
 /**
  * Created by anweshmishra on 22/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class TicTacBoxView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
