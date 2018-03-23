076e578a984daed377cc5004137f63e7d696abb3
Anwesh43 <anweshthecool0@gmail.com>
2018-03-23 08:16:43 +0530

    adding fulllScreen support

--- app/src/main/java/ui/anwesome/com/kotlintictacboxview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintictacboxview/MainActivity.kt
@@ -2,11 +2,18 @@ package ui.anwesome.com.kotlintictacboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import ui.anwesome.com.tictacboxview.TicTacBoxView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        s
+        TicTacBoxView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
